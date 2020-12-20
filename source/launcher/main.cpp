bbaa4966340575aa3511d47f977554f4d54209a5
Hanno <h.baensch.92@gmail.com>
2020-12-20 20:05:12 +0100

    Only spawn console when specified on cmd line

--- source/launcher/main.cpp
+++ source/launcher/main.cpp
@@ -1,17 +1,49 @@
 #include <Windows.h>
-#include <iostream>
 
 #include <detours/detours.h>
 
 #include <fmt/format.h>
 #include <structopt/app.hpp>
 
+enum ReturnReason {
+	SUCCESS,
+	FAILED_SPAWNING_GAME_PROCESS,
+	FAILED_PARSING_COMMAND_LINE,
+	FAILED_CREATING_PIPES,
+	FAILED_SETTING_READ_PIPE_PROPERTIES,
+	FAILED_CREATING_CONSOLE,
+	FAILED_DESTROYING_CONSOLE,
+};
+
 struct CommandLineOptions {
 	std::optional<std::string> exe_dir;
+	std::optional<bool> console = false;
 };
-VISITABLE_STRUCT(CommandLineOptions, exe_dir);
+VISITABLE_STRUCT(CommandLineOptions, exe_dir, console);
 
 static HANDLE s_Process = NULL;
+static FILE* s_ConsoleStream = NULL;
+
+bool CreateConsole() {
+	if (AllocConsole() == FALSE)
+		return false;
+
+	if (SetConsoleTitle("Playlunky") == FALSE)
+		return false;
+
+	if (freopen_s(&s_ConsoleStream, "CONOUT$", "w+", stdout) != 0)
+		return false;
+
+	return true;
+}
+
+bool DestroyConsole() {
+	if (fclose(s_ConsoleStream) != 0)
+		return false;
+	if (FreeConsole() == FALSE)
+		return false;
+	return true;
+}
 
 BOOL WINAPI HandlerRoutine(DWORD dwCtrlType)
 {
@@ -30,10 +62,18 @@ BOOL WINAPI HandlerRoutine(DWORD dwCtrlType)
 	return TRUE;
 }
 
-int main(int argc, char* argv[])
+int WinMain(
+	[[maybe_unused]] HINSTANCE hInstance, [[maybe_unused]] HINSTANCE hPrevInstance,
+	[[maybe_unused]] LPSTR lpCmdLine, [[maybe_unused]] int nShowCmd)
 {
 	try {
-		auto options = structopt::app("playlunky_launcher").parse<CommandLineOptions>(argc, argv);
+		auto options = structopt::app("playlunky_launcher").parse<CommandLineOptions>(__argc, __argv);
+
+		if (options.console.value_or(false)) {
+			if (!CreateConsole()) {
+				return FAILED_CREATING_CONSOLE;
+			}
+		}
 
 		SetConsoleCtrlHandler(HandlerRoutine, TRUE);
 
@@ -60,11 +100,11 @@ int main(int argc, char* argv[])
 		HANDLE out_write = 0;
 
 		if (!CreatePipe(&out_read, &out_write, &sa, 0)) {
-			return -1;
+			return FAILED_CREATING_PIPES;
 		}
 
 		if (!SetHandleInformation(out_read, HANDLE_FLAG_INHERIT, 0)) {
-			return -1;
+			return FAILED_SETTING_READ_PIPE_PROPERTIES;
 		}
 
 		STARTUPINFOA si{};
@@ -89,19 +129,27 @@ int main(int argc, char* argv[])
 			while (ReadFile(out_read, buffer, 1024, &read, NULL))
 			{
 				buffer[read] = '\0';
-				std::cout << buffer;
+				fmt::print(buffer);
 			}
 
 			CloseHandle(out_read);
-			return 0;
+
+			if (options.console.value_or(false)) {
+				if (!DestroyConsole()) {
+					return FAILED_DESTROYING_CONSOLE;
+				}
+			}
+
+			return SUCCESS;
 		}
 
 		fmt::print("Failed to spawn process: {}, Error code: {}\n", exe_path, GetLastError());
+		return FAILED_SPAWNING_GAME_PROCESS;
 	}
 	catch (structopt::exception& e) {
 		fmt::print(e.what());
 		fmt::print(e.help());
 	}
 
-	return -1;
+	return FAILED_PARSING_COMMAND_LINE;
 }
