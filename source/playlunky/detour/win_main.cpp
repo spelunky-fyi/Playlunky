cc8e5d95ced8a799986b89d6c9c9b9c9104a2391
Hanno <h.baensch.92@gmail.com>
2020-12-25 09:24:27 +0100

    Improve version string finding

--- source/playlunky/detour/win_main.cpp
+++ source/playlunky/detour/win_main.cpp
@@ -19,9 +19,8 @@ struct DetourWinMain
 		.Signature = "\x40\x53\x48\x83\xec\x20\x49\x8b\xd8\xff\x15\x2a\x2a\x2a\x2a\x48\x8b\xc8\xba\x02\x00\x00\x00"
 	};
 	static int Detour(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-		if (void* production_str = SigScan::FindPattern("Production", false)) {
-			const char* version_string = static_cast<const char*>(production_str) - 16;
-
+		if (void* version_address = SigScan::FindPattern("1.\x2a\x2a.\x2a\x2a\x00", false)) {
+			const char* version_string = static_cast<const char*>(version_address);
 			if (version_string != s_SupportedSpelunkyVersion) {
 				const auto message = fmt::format("This version of Spelunky 2 is not supported by Playlunky, "
 					"please update your game or check if there is a new version of Playlunky available.\n"
@@ -29,6 +28,13 @@ struct DetourWinMain
 					"Supported Version: {}", version_string, s_SupportedSpelunkyVersion);
 				MessageBox(NULL, message.c_str(), "Version Mistmacht Detected", MB_OK);
 			}
+		}
+		else {
+			const auto message = fmt::format("This version of Spelunky 2 is not supported by Playlunky, "
+				"please update your game or check if there is a new version of Playlunky available.\n"
+				" Spelunky Version: could not be verified\n"
+				"Supported Version: {}", s_SupportedSpelunkyVersion);
+			MessageBox(NULL, message.c_str(), "Version Mistmacht Detected", MB_OK);
 		}
 
 		return Trampoline(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
