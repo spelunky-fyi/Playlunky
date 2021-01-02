f6b076f4e100d20d36d085c047d99e685ab09252
Hanno <h.baensch.92@gmail.com>
2021-01-02 23:29:38 +0100

    Switch from MurmurHash3 to crc32

--- source/playlunky/mod/string_hash.cpp
+++ source/playlunky/mod/string_hash.cpp
@@ -1,20 +1,17 @@
 #include "string_hash.h"
 
-#include "third-party/MurmurHash3.h"
 #include "util/algorithms.h"
 #include "util/regex.h"
 
 #include <array>
 #include <fstream>
 #include <span>
+#include <zlib.h>
 
 static constexpr ctll::fixed_string s_CommentBlockRule{ "^#+" };
 
 std::uint32_t HashString(std::string_view string) {
-	static constexpr std::uint64_t mask = static_cast<std::uint64_t>(~static_cast<std::uint32_t>(0x0));
-	std::array<std::uint64_t, 2> hash_128;
-	MurmurHash3_x64_128(string.data(), static_cast<int>(string.size()), 0xD67FADD1, &hash_128);
-	return static_cast<std::uint32_t>(mask & hash_128.back());
+	return crc32(0, reinterpret_cast<const Bytef*>(string.data()), static_cast<std::uint32_t>(string.size()));
 }
 
 bool CreateHashedStringsFile(const std::filesystem::path& source_file, const std::filesystem::path& destination_file) {
