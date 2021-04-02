7fe8c59d8dde9a701483cce3293d215fc93c656d
Hanno <h.baensch.92@gmail.com>
2021-04-02 20:14:40 +0200

    Bump db version

--- source/playlunky/mod/mod_database.cpp
+++ source/playlunky/mod/mod_database.cpp
@@ -10,9 +10,9 @@
 //		0xD00FAFFE -- v0.4.1
 //		0xABAD1DEA -- v0.4.2
 //		0xBADAB000 -- v0.5.6
-//		0xABCDEF16 -- v0.7.0 (beta)
-//		0xABACAB00 -- v0.7.0 (latest)
-static constexpr std::uint32_t s_ModDatabaseMagicNumber{ 0xABCDEF16 };
+//		0xABCDEF16 -- v0.7.0
+//		0xABACAB00 -- v0.7.1
+static constexpr std::uint32_t s_ModDatabaseMagicNumber{ 0xABACAB00 };
 
 ModDatabase::ModDatabase(std::filesystem::path database_folder, std::filesystem::path mod_folder, ModDatabaseFlags flags)
 	: mDatabaseFolder(std::move(database_folder))
