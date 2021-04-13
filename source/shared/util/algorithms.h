2561b7058f84fee7d2b5a65433e8686b20aba8f7
Malacath <h.baensch.92@gmail.com>
2021-04-13 07:40:00 +0200

    Fix algos to work with member begin/end

--- source/shared/util/algorithms.h
+++ source/shared/util/algorithms.h
@@ -1,46 +1,24 @@
 #pragma once
 
+#include "concepts.h"
+
 #include <filesystem>
-#include <type_traits>
 #include <string_view>
 #include <utility>
 
 namespace algo {
-	template<class T, class U>
-	concept is_comparable = requires(T&& lhs, T&& rhs) {
-		lhs == rhs;
-		rhs == lhs;
-	};
-	
-	template<class T>
-	concept range = requires (T&& cont) {
-		begin(cont);
-		end(cont);
-	};
-
-	template<class T>
-	requires range<T>
-	using range_element_t = decltype(*begin(std::declval<T>()));
-	template<class T>
-	requires range<T>
-	using range_value_t = std::remove_reference_t<decltype(*begin(std::declval<T>()))>;
-	
-	template<class T, class U>
-	requires range<T>
-	inline constexpr bool range_contains_v = std::is_same_v<std::decay_t<range_element_t<T>>, std::decay_t<U>>;
-
 	template<class ContainerT, class FunT>
-	requires range<ContainerT> && std::is_invocable_v<FunT, range_element_t<ContainerT>>
+	requires range<ContainerT> && std::is_invocable_r_v<bool, FunT, range_element_t<ContainerT>>
 	void erase_if(ContainerT&& container, FunT&& fun) {
-		const auto begin_it = begin(container);
-		const auto end_it = end(container);
+		const auto begin_it = get_begin(container);
+		const auto end_it = get_end(container);
 		container.erase(std::remove_if(begin_it, end_it, std::forward<FunT>(fun)), end_it);
 	}
 	template<class ContainerT, class ValueT>
 	requires range<ContainerT> && is_comparable<range_element_t<ContainerT>, ValueT>
 	void erase(ContainerT&& container, ValueT&& value) {
-		const auto begin_it = begin(container);
-		const auto end_it = end(container);
+		const auto begin_it = get_begin(container);
+		const auto end_it = get_end(container);
 		container.erase(std::remove(begin_it, end_it, std::forward<ValueT>(value)), end_it);
 	}
 	template<class ContainerT, class T, class U, class V>
@@ -52,10 +30,10 @@ namespace algo {
 	}
 
 	template<class ContainerT, class FunT>
-	requires range<ContainerT> && std::is_invocable_v<FunT, range_element_t<ContainerT>>
+	requires range<ContainerT> && std::is_invocable_r_v<bool, FunT, range_element_t<ContainerT>>
 	auto find_if(ContainerT&& container, FunT&& fun) -> range_value_t<ContainerT>* {
-		const auto begin_it = begin(container);
-		const auto end_it = end(container);
+		const auto begin_it = get_begin(container);
+		const auto end_it = get_end(container);
 		auto found_it = std::find_if(begin_it, end_it, std::forward<FunT>(fun));
 		if (found_it != end_it) {
 			return &*found_it;
@@ -65,8 +43,8 @@ namespace algo {
 	template<class ContainerT, class ValueT>
 	requires range<ContainerT> && is_comparable<range_element_t<ContainerT>, ValueT>
 	auto find(ContainerT&& container, ValueT&& value) -> range_value_t<ContainerT>* {
-		const auto begin_it = begin(container);
-		const auto end_it = end(container);
+		const auto begin_it = get_begin(container);
+		const auto end_it = get_end(container);
 		auto found_it = std::find(begin_it, end_it, std::forward<ValueT>(value));
 		if (found_it != end_it) {
 			return &*found_it;
@@ -82,7 +60,7 @@ namespace algo {
 	}
 
 	template<class ContainerT, class FunT>
-	requires range<ContainerT> && std::is_invocable_v<FunT, range_element_t<ContainerT>>
+	requires range<ContainerT> && std::is_invocable_r_v<bool, FunT, range_element_t<ContainerT>>
 	bool contains_if(ContainerT&& container, FunT&& fun) {
 		return find_if(std::forward<ContainerT>(container), std::forward<FunT>(fun)) != nullptr;
 	}
@@ -96,23 +74,23 @@ namespace algo {
 	template<class ContainerT, class ValueT>
 	requires range<ContainerT> && is_comparable<range_element_t<ContainerT>, ValueT>
 	bool contains(ContainerT&& container, ValueT&& value) {
-		const auto begin_it = begin(container);
-		const auto end_it = end(container);
+		const auto begin_it = get_begin(container);
+		const auto end_it = get_end(container);
 		return std::find(begin_it, end_it, std::forward<ValueT>(value)) != end_it;
 	}
 
 	template<class ContainerT, class ValueT>
 	requires range<ContainerT> && is_comparable<range_element_t<ContainerT>, ValueT>
 	auto count(ContainerT&& container, ValueT&& value) {
-		const auto begin_it = begin(container);
-		const auto end_it = end(container);
+		const auto begin_it = get_begin(container);
+		const auto end_it = get_end(container);
 		return static_cast<std::size_t>(std::count(begin_it, end_it, std::forward<ValueT>(value)));
 	}
 	template<class ContainerT, class FunT>
-	requires range<ContainerT> && std::is_invocable_v<FunT, range_element_t<ContainerT>>
+	requires range<ContainerT> && std::is_invocable_r_v<bool, FunT, range_element_t<ContainerT>>
 	auto count_if(ContainerT&& container, FunT&& fun) {
-		const auto begin_it = begin(container);
-		const auto end_it = end(container);
+		const auto begin_it = get_begin(container);
+		const auto end_it = get_end(container);
 		return static_cast<std::size_t>(std::count_if(begin_it, end_it, std::forward<FunT>(fun)));
 	}
 
---
+++ source/shared/util/concepts.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <filesystem>
+#include <type_traits>
+#include <string_view>
+#include <utility>
+
+namespace algo {
+	template<class T, class U>
+	concept is_comparable = requires(T&& lhs, T&& rhs) {
+		lhs == rhs;
+		rhs == lhs;
+	};
+
+	template<class T>
+	concept adl_range = requires (T && cont) {
+		begin(cont);
+		end(cont);
+	};
+	template<class T>
+	concept not_adl_range = !requires (T && cont) {
+		begin(cont);
+		end(cont);
+	};
+	template<class T>
+	concept member_range = requires (T&& cont) {
+		cont.begin();
+		cont.end();
+	};
+	template<class ContainerT>
+	requires member_range<ContainerT> && not_adl_range<ContainerT>
+	constexpr auto get_begin(ContainerT&& cont) {
+		return cont.begin();
+	}
+	template<class ContainerT>
+	requires member_range<ContainerT> && not_adl_range<ContainerT>
+	constexpr auto get_end(ContainerT&& cont) {
+		return cont.end();
+	}
+	template<class ContainerT>
+	requires adl_range<ContainerT>
+	constexpr auto get_begin(ContainerT&& cont) {
+		return begin(cont);
+	}
+	template<class ContainerT>
+	requires adl_range<ContainerT>
+	constexpr auto get_end(ContainerT&& cont) {
+		return end(cont);
+	}
+	
+	template<class T>
+	concept range = requires (T&& cont) {
+		get_begin(cont);
+		get_end(cont);
+	};
+
+	template<class T>
+	requires range<T>
+	using range_element_t = decltype(*get_begin(std::declval<T>()));
+	template<class T>
+	requires range<T>
+	using range_value_t = std::remove_reference_t<decltype(*get_begin(std::declval<T>()))>;
+	
+	template<class T, class U>
+	requires range<T>
+	inline constexpr bool range_contains_v = std::is_same_v<std::decay_t<range_element_t<T>>, std::decay_t<U>>;
+}
