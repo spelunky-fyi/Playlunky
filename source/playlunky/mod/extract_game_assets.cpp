84b92c9eb43e6daf1b0d49fdb50246e1e99a8987
Malacath <h.baensch.92@gmail.com>
2021-04-15 22:54:07 +0200

    Be a bit explicit about asset extraction taking some time

--- source/playlunky/mod/extract_game_assets.cpp
+++ source/playlunky/mod/extract_game_assets.cpp
@@ -45,7 +45,7 @@ bool ExtractGameAssets(std::span<const std::filesystem::path> files, const std::
         return true;
     }
 
-    LogInfo("Extracting required game assets from Spel2.exe...");
+    LogInfo("Extracting required game assets from Spel2.exe, this might take a few minutes...");
 
 	if (void* data = SigScan::GetDataSection()) {
         struct Asset {
