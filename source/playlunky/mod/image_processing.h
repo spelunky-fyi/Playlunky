0d0e5df9b8b6d638845f257625185ffaf59976c4
Malacath <h.baensch.92@gmail.com>
2022-01-14 08:55:58 +0100

    Add util for luminance scaling

--- source/playlunky/mod/image_processing.h
+++ source/playlunky/mod/image_processing.h
@@ -13,6 +13,7 @@ Image MakeCombinedMenuPetHeads(std::vector<std::pair<Image, std::filesystem::pat
 Image AlphaBlend(Image lhs_image, Image rhs_image);
 Image ColorBlend(Image color_image, Image target_image);
 Image LuminanceBlend(Image luminance_image, Image target_image);
+Image LuminanceScale(Image luminance_scale_image, Image target_image);
 
 Image ReplaceColor(Image input_image, ColorRGB8 source_color, ColorRGB8 target_color);
 Image ReplaceColors(Image input_image, const std::vector<ColorRGB8>& source_colors, const std::vector<ColorRGB8>& target_colors);
